91ce079e354252b790ddee8c6d9bb0dfeb6720e4
Jason Colobong <jasoncolobong@outlook.com>
2022-02-02 08:58:14 -0600

    installed framework 6.0 to API layer

--- ToDoAPI.API/Controllers/ToDoController.cs
+++ ToDoAPI.API/Controllers/ToDoController.cs
@@ -1,12 +1,12 @@
-﻿using ToDoAPI.API.Models;
-using ToDoAPI.DATA.EF;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Cors;
+using ToDoAPI.API.Models;
+using ToDoAPI.DATA.EF;
 
 namespace ToDoAPI.API.Controllers
 {
@@ -23,13 +23,13 @@ namespace ToDoAPI.API.Controllers
                 ToDoId = t.ToDoId,
                 Action = t.Action,
                 Done = t.Done,
-                CategoryId = t.CategoryId,
-            Category = new CategoryViewModel()
-            {
-                CategoryId = t.Category.CategoryId,
-                CategoryName = t.Category.CategoryName,
-                CategoryDescription = t.Categroy.CategoryDescription
-            }
+                CategoryId = t.CategoryID,
+                Category = new CategoryViewModel()
+                {
+                    CategoryId = t.Category.CategoryID,
+                    CategoryName = t.Category.CategoryName,
+                    CategoryDescription = t.Category.CategoryDescription
+                }
 
 
             }).ToList<ToDoViewModel>();
@@ -42,6 +42,30 @@ namespace ToDoAPI.API.Controllers
             return Ok(toDo);
 
         }//end GetToDo
+
+        public IHttpActionResult GetToDo(int id)
+        {
+            ToDoViewModel toDo = db.ToDoItems.Include("Category").Where(t => t.ToDoId == id).Select(t => new ToDoViewModel()
+            {
+                ToDoId = t.ToDoId,
+                Action = t.Action,
+                Done = t.Done,
+                CategoryId = t.CategoryId,
+                Category = new CategoryViewModel()
+                {
+                    CategoryId = t.Category.CategoryID,
+                    CategoryName = t.Category.CategoryName,
+                    CategoryDescription = t.Category.CategoryDescription
+                }
+            }).FirstOrDefault();
+
+            if (toDo == null)
+                return NotFound();
+
+            return Ok(toDo);
+
+
+        }//end GetResource
 
 
 
