1bb05e529baf6e55d7b620efe6eb2d58d938f21e
Jason Colobong <jasoncolobong@outlook.com>
2022-02-01 22:12:13 -0600

    added functionality to API layer

---
+++ ToDoAPI.API/Controllers/ToDoController.cs
@@ -0,0 +1,49 @@
+﻿using ToDoAPI.API.Models;
+using ToDoAPI.DATA.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace ToDoAPI.API.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class ToDoController : ApiController
+    {
+
+        ToDoEntities db = new ToDoEntities();
+
+        public IHttpActionResult GetToDo()
+        {
+            List<ToDoViewModel> toDo = db.ToDoItems.Include("Category").Select(t => new ToDoViewModel()
+            {
+                ToDoId = t.ToDoId,
+                Action = t.Action,
+                Done = t.Done,
+                CategoryId = t.CategoryId,
+            Category = new CategoryViewModel()
+            {
+                CategoryId = t.Category.CategoryId,
+                CategoryName = t.Category.CategoryName,
+                CategoryDescription = t.Categroy.CategoryDescription
+            }
+
+
+            }).ToList<ToDoViewModel>();
+
+            if (toDo.Count == 0)
+
+            {
+                return NotFound();
+            }//end if
+            return Ok(toDo);
+
+        }//end GetToDo
+
+
+
+    }//end class
+}//end namespace
---
+++ ToDoAPI.API/Models/ToDoAPIViewModels.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations; //Added for access to annotations
+
+namespace ToDoAPI.API.Models
+{
+    public class ToDoViewModel
+    {
+        [Key]
+        public int ToDoId { get; set; }
+        public string Action { get; set; }
+        public bool Done { get; set; }
+        public int CategoryId { get; set; }
+
+        public virtual CategoryViewModel Category { get; set; }
+
+    }
+
+    public class CategoryViewModel
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryDescription { get; set; }
+    }
+}
